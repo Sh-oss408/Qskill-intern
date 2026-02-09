fcc7a785e160330a5f3309436f5874582f18e31a
Sh-oss408 <shravishukla123@gmail.com>
2026-02-10 02:01:12 +0530

    Refactor translateTextApi function for API change

--- react-intern-task/src/utils/translate.js
+++ react-intern-task/src/utils/translate.js
@@ -1,10 +1,22 @@
-export async function translateTextApi(text, targetLang) {
+export const translateTextApi = async (text, language) => {
+  const encodedParams = new URLSearchParams();
+  encodedParams.append("q", text);
+  encodedParams.append("target", language);
+  encodedParams.append("source", "en");
+
   const response = await fetch(
-    `https://api.mymemory.translated.net/get?q=${encodeURIComponent(
-      text
-    )}&langpair=en|${targetLang}`
+    "https://google-translate1.p.rapidapi.com/language/translate/v2",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Key": "YOUR_KEY_HERE",
+        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+      },
+      body: encodedParams,
+    }
   );
 
-  const data = await response.json();
-  return data.responseData.translatedText;
-}
+  const result = await response.json();
+  return result.data.translations[0].translatedText;
+};
