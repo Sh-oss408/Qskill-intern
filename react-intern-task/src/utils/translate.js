c93034c24807a4eabbdff11f6e867375b79dc97c
Sh-oss408 <shravishukla123@gmail.com>
2026-02-10 02:21:50 +0530

    Update translate.js

--- react-intern-task/src/utils/translate.js
+++ react-intern-task/src/utils/translate.js
@@ -5,13 +5,13 @@ export const translateTextApi = async (text, language) => {
   encodedParams.append("source", "en");
 
   const response = await fetch(
-    "https://google-translate1.p.rapidapi.com/language/translate/v2",
+    "https://google-translate112.p.rapidapi.com/lang-detect?text=%E5%9B%BD%E8%AF%AD%2F%E5%9C%8B%E8%AA%9E",
     {
       method: "POST",
       headers: {
         "content-type": "application/x-www-form-urlencoded",
         "X-RapidAPI-Key": "ac7295702dmsh1df2c3f434ae019p12a205jsn52b6e710b30b",
-        "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+        "X-RapidAPI-Host": "google-translate112.p.rapidapi.com",
       },
       body: encodedParams,
     }
